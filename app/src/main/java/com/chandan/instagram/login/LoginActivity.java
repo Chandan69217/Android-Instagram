9c4f3f5d0171ce3e2eddf7c316a60aa7cea65501
Chandan Sharma <chandansharma69217@gmail.com>
2023-12-18 22:32:58 +0530

    Initialized Repository

---
+++ app/src/main/java/com/chandan/instagram/login/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.chandan.instagram.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chandan.instagram.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/chandan/instagram/splashscreen/SplashActivity.java
@@ -0,0 +1,42 @@
+package com.chandan.instagram.splashscreen;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.WindowDecorActionBar;
+import androidx.core.view.WindowCompat;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+
+import com.chandan.instagram.MainActivity;
+import com.chandan.instagram.R;
+import com.chandan.instagram.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(),false);
+
+       new Handler().postDelayed(new Runnable() {
+           @Override
+           public void run() {
+               Intent NextActivity;
+               SharedPreferences pref = getSharedPreferences("login",MODE_PRIVATE);
+               boolean check = pref.getBoolean("flag",false);
+               if(check){
+                   NextActivity = new Intent(getApplicationContext(), MainActivity.class);
+               }else{
+                   NextActivity = new Intent(getApplicationContext(), LoginActivity.class);
+               }
+               startActivity(NextActivity);
+               finish();
+           }
+       },4000);
+    }
+}
