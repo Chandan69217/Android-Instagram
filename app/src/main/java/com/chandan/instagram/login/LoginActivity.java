8e302463b965f9bca1afe29ca6244c4b22b71d34
Chandan Sharma <chandansharma69217@gmail.com>
2023-12-19 16:20:47 +0530

    Design Login Activity

--- app/src/main/java/com/chandan/instagram/login/LoginActivity.java
+++ app/src/main/java/com/chandan/instagram/login/LoginActivity.java
@@ -3,11 +3,11 @@ package com.chandan.instagram.login;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 import com.chandan.instagram.R;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
